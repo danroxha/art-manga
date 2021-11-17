794369ccb7d6f30797c5cd5f9e8c2d166bcf6b2e
dlrv <daniel-rocha.silva@gft.com>
2021-11-17 09:18:35 -0300

    chore: Adicionado arquivos

---
+++ src/test/java/com/art/manga/MangaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.art.manga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
